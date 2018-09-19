2a7ffb771ca60dbca12431adf2fb655587db284e
aman2500 <apatel2500@gmail.com>
2018-09-19 16:03:42 -0500

    lab3

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(WebScraper.urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
+        System.out.println(WebScraper.urlToString("http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt"));
+    }
+}

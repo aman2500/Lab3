c1d01b7cf89f0e830120e222da586bba1d4e4dba
aman2500 <apatel2500@gmail.com>
2018-09-19 16:24:22 -0500

    lab3 done?

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
-        System.out.println(WebScraper.urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
-        System.out.println(WebScraper.urlToString("http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt"));
+        String w1 = WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String w2 = WebScraper.urlToString("https://www.bls.gov/tus/charts/chart9.txt");
+        String w3 = WebScraper.urlToString("http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt");
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        for (char current : w1.toCharArray()){
+            if (current == ' ') {
+                c1 += 1;
+            }
+        }
+        for (char current : w2.toCharArray()){
+            if (current == ' ') {
+                c2 += 1;
+            }
+        }
+        for (char current : w3.toCharArray()){
+            if (current == ' ') {
+                c3 += 1;
+            }
+        }
+        System.out.println(c1 + " " + c2 + " " + c3);
     }
 }
